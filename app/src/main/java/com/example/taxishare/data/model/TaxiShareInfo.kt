2f6c08f1d38cb4a24d7993060081bc080953b3f7
MyeongByeol <kokoxg2@naver.com>
2019-07-08 13:51:21 +0900

    issue-8/taxiShareEntity 수정
    taxiShareEntity 구분을 위해 id 추가

--- app/src/main/java/com/example/taxishare/data/model/TaxiShareInfo.kt
+++ app/src/main/java/com/example/taxishare/data/model/TaxiShareInfo.kt
@@ -7,6 +7,7 @@ package com.example.taxishare.data.model
 import androidx.recyclerview.widget.DiffUtil
 
 data class TaxiShareInfo(
+    val id : String,
     val title: String, val limit: Int,
     val description: String, val startLocation: Location,
     val endLocation: Location
@@ -14,16 +15,10 @@ data class TaxiShareInfo(
     companion object {
         val DIFF_UTIL: DiffUtil.ItemCallback<TaxiShareInfo> = object : DiffUtil.ItemCallback<TaxiShareInfo>() {
             override fun areItemsTheSame(oldItem: TaxiShareInfo, newItem: TaxiShareInfo): Boolean =
-                (oldItem.startLocation.latitude == newItem.startLocation.latitude) &&
-                        (oldItem.startLocation.longitude == newItem.startLocation.longitude) &&
-                        (oldItem.endLocation.longitude == newItem.endLocation.longitude) &&
-                        (oldItem.endLocation.latitude == newItem.endLocation.latitude)
+                oldItem.id == newItem.id
 
             override fun areContentsTheSame(oldItem: TaxiShareInfo, newItem: TaxiShareInfo): Boolean =
-                (oldItem.startLocation.latitude == newItem.startLocation.latitude) &&
-                        (oldItem.startLocation.longitude == newItem.startLocation.longitude) &&
-                        (oldItem.endLocation.longitude == newItem.endLocation.longitude) &&
-                        (oldItem.endLocation.latitude == newItem.endLocation.latitude)
+                oldItem.id == newItem.id
         }
     }
 }
