9e7d2baf096a9ed6a68c60c670b5d5295c05e161
MyeongByeol <kokoxg2@naver.com>
2019-07-18 14:51:29 +0900

    issue-12/taxi_share_list item xml create
    택시 공유 item xml 작성

--- app/src/main/java/com/example/taxishare/view/main/taxisharelist/TaxiShareListFragment.kt
+++ app/src/main/java/com/example/taxishare/view/main/taxisharelist/TaxiShareListFragment.kt
@@ -32,10 +32,8 @@ class TaxiShareListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_taxi_share_list, container, false)
-    }
+    ): View? = inflater.inflate(R.layout.fragment_taxi_share_list, container, false)
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
